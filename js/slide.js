34f15a44d6f804d8a038447eb0b669b9497d20e3
kcy230426 <purple_galaxy@naver.com>
2023-07-27 18:05:00 +0900

    디자인만 수정하면댐

--- js/slide.js
+++ js/slide.js
@@ -1,8 +1,23 @@
 $(function(){
-    setInterval(
-        $("slide-box").animate({"marginLeft":-1320},400, function(){
+    setInterval(function(){
+        $("#slide-box").animate({"marginLeft":-1320},400, function(){
             $(this).find("li").eq(0).appendTo(this)
             $(this).css("margin-Left",0)
-        }),3000
+        })   
+    },3000
     )
 })
+
+// $(function(){
+ 
+//     setInterval(function(){
+     
+//       $("#slide ul").animate({"marginLeft" : -800 }, 400, function(){
+//         $(this).find('li').eq(0).appendTo($(this))
+//         $(this).css('margin-left',0)
+  
+//       })
+//     },3000);
+//   })
+  
+  
