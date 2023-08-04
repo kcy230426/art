2cec4d2bc2c43005db5f1796fea2d67a566258c0
kcy230426 <purple_galaxy@naver.com>
2023-08-04 18:03:59 +0900

    슬라이드 식 수정

--- js/slide.js
+++ js/slide.js
@@ -1,8 +1,8 @@
 $(function(){
-    setInterval(
+    setInterval(function(){
         $("#slide .slidebox").animate({marginLeft:-1200},400,function(){
             $(this).find("li").eq(0).appendTo.$(this);
             $(this).css("margin-left", 0);
-        },3000)
-    )
+        })
+    },3000)
 })
