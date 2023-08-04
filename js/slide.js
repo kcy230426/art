927db432d9fce29bb1e81a322e06325e27e150a7
kcy230426 <purple_galaxy@naver.com>
2023-08-04 18:01:48 +0900

    슬라이드

---
+++ js/slide.js
@@ -0,0 +1,8 @@
+$(function(){
+    setInterval(
+        $("#slide .slidebox").animate({marginLeft:-1200},400,function(){
+            $(this).find("li").eq(0).appendTo.$(this);
+            $(this).css("margin-left", 0);
+        },3000)
+    )
+})
