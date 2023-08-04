0989eff1f8da882a5dd51e648dffb3e773eb5de8
kcy230426 <purple_galaxy@naver.com>
2023-08-04 15:29:56 +0900

    자바스크립트 작업 완료

--- js/slide.js
+++ js/slide.js
@@ -5,7 +5,28 @@ $(function(){
             $(this).css("margin-left", "0")
         })
     },3000)
-    $("body").on("click", "#threeTab .ntitle", function(){
+    $("body").on("click", "#threeTab .title", function(){
         $(this).addClass("on");
+        $(this).siblings().removeClass("on");
+    })
+    $("body").on("click", "#threeTab .ntitle", function(){
+        $("#threeTab .ulbox .nul").addClass("on");
+        $("#threeTab .ulbox .nul").siblings().removeClass("on");
+    })
+    $("body").on("click", "#threeTab .gtitle", function(){
+        $("#threeTab .ulbox .gul").addClass("on");
+        $("#threeTab .ulbox .gul").siblings().removeClass("on");
+    })
+    $("body").on("click",".nul li", function(){
+        $(".popup").removeClass("d-none").addClass("d-block")
+    })
+    $("body").on("click",".gul li", function(){
+        $(".gallerypop").removeClass("d-none").addClass("d-block")
+    })
+    $("body").on("click",".popup button", function(){
+        $(".popup").removeClass("d-block").addClass("d-none")
+    })
+    $("body").on("click",".gallerypop button", function(){
+        $(".gallerypop").removeClass("d-block").addClass("d-none")
     })
 })
