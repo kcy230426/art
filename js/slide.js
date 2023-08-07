a9bad340784d399ce7e77a02a2eab36efde105ec
kcy230426 <purple_galaxy@naver.com>
2023-08-07 18:06:38 +0900

    슬라이드 수정 완료

--- js/slide.js
+++ js/slide.js
@@ -1,8 +1,16 @@
 $(function(){
     setInterval(function(){
-        $("#slide .slidebox").animate({marginLeft:-800} -600 ,function(){
+        $("#slide .slidebox").animate({marginLeft:-800}, 600 ,function(){
             $(this).find("li").eq(0).appendTo(this);
             $(this).css("margin-left","0");
         })
     },3000)
 })
+
+// $(function(){
+//     setInterval(function(){
+//         $("#slide .slidebox").animate({marginLeft:-800},400,function(){
+//             $(this).find("li").eq(0).appendTo(this);
+//             $(this).css("margin-left", "0");
+//         })},3000)
+// })
